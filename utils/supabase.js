150ccc496fe899999bccdfe46bdede92f49bfe00
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-06-17 22:11:05 +0530

    Config

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,6 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
+import { defineNuxtConfig } from 'nuxt3'
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseAnonKey = process.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      secret: process.env.SUPABASE_URL,
+    }
+  }
+})
