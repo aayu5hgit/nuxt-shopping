5ef23bd3e2ee0f24579391b5a1513a20beabe9f8
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-06-17 22:25:37 +0530

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
-  runtimeConfig: {
-    public: {
-      secret: process.env.SUPABASE_URL,
-    }
-  }
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', {
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey: process.env.SUPABASE_KEY
+  }]
+  
 })
 
 
--- utils/supabase.js
+++ utils/supabase.js
@@ -1,15 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-import { defineNuxtConfig } from 'nuxt3'
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default defineNuxtConfig({
-  runtimeConfig: {
-    public: {
-      secret: process.env.SUPABASE_URL,
-    }
-  }
-})
