542abd8aaef4c84e6fb5793a9c10374ced4d6425
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-06-17 22:35:06 +0530

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', {
-    supabaseUrl: process.env.SUPABASE_URL,
-    supabaseKey: process.env.SUPABASE_KEY
-  }]
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase']
   
 })
 
