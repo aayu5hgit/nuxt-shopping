9734992503e69ca7990425a64dcd203bda158033
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-06-17 22:14:30 +0530

    Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+  runtimeConfig: {
+    public: {
+      secret: process.env.SUPABASE_URL,
+    }
+  }
 })
+
+
